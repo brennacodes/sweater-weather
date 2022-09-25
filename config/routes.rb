abb6b5657381cba311ed5aceb854db74742fcfb9
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-25 16:39:24 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get '/forecast', to: 'forecasts#show', as: 'forecasts'
+      resources :users, only: [:create]
+      resources :road_trips, only: [:create], as: :road_trips
+    end
+  end
 end
