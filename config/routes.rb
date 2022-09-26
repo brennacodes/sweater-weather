28bf3fa04608bd8331bcb06aae233ef24ee40108
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-26 09:30:03 -0600

    Add book-search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/book-search', to: 'books#index', as: 'books'
       get '/forecast', to: 'forecasts#show', as: 'forecasts'
       resources :users, only: [:create]
       resources :road_trips, only: [:create], as: :road_trips
