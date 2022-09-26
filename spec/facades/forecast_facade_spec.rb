b992befd272bbfc3aee60d91168f939f5249c5e4
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-26 17:39:01 -0600

    Update forecast facade spec

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ForecastFacade, type: :facade, vcr: true do
+  let!(:coords) { [
+    39.738453,
+    -104.984853
+  ] }
+  let!(:units) { 'imperial' }
+
+  describe "class methods" do
+    describe "weather" do
+      it "returns a forecast object" do
+        forecast = ForecastFacade.weather(location)
+        expect(forecast).to be_a(Forecast)
+      end
+    end
+
+    describe "current_conditions" do
+      it "returns a current weather object" do
+        current = ForecastFacade.current_conditions(coords)
+        expect(current).to be_a(CurrentWeather)
+      end
+    end
+  end
+end
