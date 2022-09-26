d84911f98a684821124362065fd33cd002b703c9
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-26 09:31:40 -0600

    Add index, book_params to books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,17 @@
 module Api
   module V1
     class Api::V1::BooksController < ApplicationController
+      def index
+        location = book_params[:location]
+        quantity = book_params[:quantity]
+        books = BookFacade.new(location, quantity)
+        render json: BookSerializer.new(books)
+      end
+
+      private
+        def book_params
+          params.permit(:location, :quantity)
+        end
     end
   end
 end
