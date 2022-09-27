590fd5ba1fee8cc769d850f7892b3bebe34cedc8
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-27 17:32:55 -0600

    Update open_weather_svc_spec

--- spec/services/open_weather_service_spec.rb
+++ spec/services/open_weather_service_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe OpenWeatherService, type: :service, vcr: { :match_requests_on => [:uri] } do
+  it "retrieves weather for given coordinates" do
+    coords = [39.738453, -104.984853]
+    units = "imperial"
 
+    response = OpenWeatherService.get_weather(coords, units)
+    expect(response).to_not be_nil
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:current)
+    expect(response[:current]).to be_a(Hash)
+    expect(response).to have_key(:daily)
+    expect(response[:daily]).to be_an(Array)
+    expect(response).to have_key(:hourly)
+    expect(response[:hourly]).to be_an(Array)
+    expect(response).to_not have_key(:minutely)
+  end
+
+  it "retrieves forecast for a given location" do
+    coords = [39.738453, -104.984853]
+    
+    response = OpenWeatherService.get_forecast(coords)
+    expect(response).to_not be_nil
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:daily)
+    expect(response[:daily]).to be_an(Array)
+    expect(response).to have_key(:hourly)
+    expect(response[:hourly]).to be_an(Array)
+    expect(response).to_not have_key(:current)
+    expect(response).to_not have_key(:minutely)
+  end
 end
