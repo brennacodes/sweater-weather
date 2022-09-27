6c6ff864bc6e89ce9f925b8d2b0ed9fa5db6d0be
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-26 18:29:36 -0600

    Update book_spec

--- spec/poros/book_spec.rb
+++ spec/poros/book_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Book, type: :poro, vcr: true do
-  let!(:json) { File.read('spec/fixtures/book.json') }
+RSpec.describe Book, type: :poro do
+  let!(:json) { File.read('spec/fixtures/book_fixture.json') }
   let!(:parsed) { JSON.parse(json, symbolize_names: true) }
   
   it "creates a ruby Book class object" do
     book_hash = parsed[:docs].first
     book = Book.new(book_hash)
     expect(book).to be_a(Book)
-    expect(book.isbn).to eq(parsed[:isbn])
-    expect(book.title).to eq(parsed[:title])
-    expect(book.publisher).to eq(parsed[:publisher])
+    expect(book.isbn).to eq(["0762507845", "9780762507849"])
+    expect(book.title).to eq("Denver, Co")
+    expect(book.publisher).to eq(["Universal Map Enterprises"])
   end
 end
