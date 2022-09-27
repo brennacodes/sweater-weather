581fee4bea003dea587cf862f69401467d0101e8
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-26 19:01:01 -0600

    Update book serializer

--- spec/requests/api/v1/road_trips_spec.rb
+++ spec/requests/api/v1/road_trips_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "RoadTrips", type: :request, :vcr do
+RSpec.describe "RoadTrips", type: :request, vcr: true do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    
   end
 end
--- spec/serializers/book_serializer_spec.rb
+++ spec/serializers/book_serializer_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe BookSerializer, type: :serializer do
-  let!(:json) { File.read('spec/fixtures/book-search/retrieves_books_for_a_given_location_and_quantity_limit.json') }
+  let!(:json) { File.read('spec/fixtures/book_fixture.json') }
   let!(:parsed) { JSON.parse(json, symbolize_names: true) }
-  let!(:book) { Book.new(parsed[:docs].first) }
-  let!(:serializer) { BookSerializer.new(book) }
-  let!(:serialize) { ActiveModelSerializers::Adapter.create(serializer) }
+  let!(:all_books) { parsed[:docs] }
+  let!(:forecast) { { summary: "Partly cloudy", temperature: "75 F" } }
+  let!(:books) { all_books.map { |book| Book.new(book) } }
 
   it 'serializes a book' do
-    expect(serialize.to_json).to eq(
-      {
-        data: {
-          id: "null",
-          type: 'book',
-          attributes: {
-            isbn: book.isbn,
-            title: book.title,
-            publisher: book.publisher
-          }
-        }
-      }.to_json
-    )
+    poro = Books.new({
+      books: books, 
+      location: "Denver,CO",
+      forecast: forecast,
+      total_books_found: parsed[:numFound]
+    })
+
+    serializer = BookSerializer.new(poro)
+
+    expect(serializer).to have_key(:data)
+    data = serializer[:data]
+
+    expect(data).to have_key(:id)
+    expect(data[:id]).to eq("null")
+    expect(data).to have_key(:type)
+    expect(data[:type]).to eq("books")
+    expect(data).to have_key(:attributes)
+    expect(data[:attributes]).to be_a(Hash)
+    expect(data[:attributes]).to have_key(:destination)
+    expect(data[:attributes][:destination]).to eq("Denver,CO")
+    expect(data[:attributes]).to have_key(:forecast)
+    expect(data[:attributes][:forecast]).to be_a(Hash)
+    expect(data[:attributes][:forecast]).to have_key(:summary)
+    expect(data[:attributes][:forecast][:summary]).to eq("Partly cloudy")
+    expect(data[:attributes][:forecast]).to have_key(:temperature)
+    expect(data[:attributes][:forecast][:temperature]).to eq("75 F")
+    expect(data[:attributes]).to have_key(:total_books_found)
+
   end
 end
